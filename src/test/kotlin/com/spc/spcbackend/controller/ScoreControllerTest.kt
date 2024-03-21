8dda6876c10e31b0d403bc9cb428b74277d6261f
Elnathan Erh <elnathan.erh@synpulse.com>
2024-03-21 16:55:44 +0800

    Add Active Profiles test so it knows to use test DB, defined in application-test.properties
    Add more tests for POST save score.

--- src/test/kotlin/com/spc/spcbackend/controller/ScoreControllerTest.kt
+++ src/test/kotlin/com/spc/spcbackend/controller/ScoreControllerTest.kt
@@ -6,18 +6,20 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@SpringBootTest
+@ActiveProfiles("test")
 class ScoreControllerTest(@Autowired val mockMvc: MockMvc) {
     @Test
     fun `when Post score then Save score`() {
-        val content = mapOf("word" to "test", "score" to 10)
+        val content = mapOf("word" to "scrupulous", "score" to 16)
         val objectMapper = ObjectMapper()
         val json = objectMapper.writeValueAsString(content)
 
@@ -27,7 +29,37 @@ class ScoreControllerTest(@Autowired val mockMvc: MockMvc) {
                 .content(json)
         )
             .andExpect(status().isOk)
-            .andExpect(jsonPath("$.word").value("test"))
-            .andExpect(jsonPath("$.score").value(10))
+            .andExpect(jsonPath("$.word").value("scrupulous"))
+            .andExpect(jsonPath("$.score").value(16))
     }
+
+    @Test
+    fun `when Post empty score then throw bad request with error`() {
+        val content = mapOf("word" to "", "score" to 10)
+        val objectMapper = ObjectMapper()
+        val json = objectMapper.writeValueAsString(content)
+
+        mockMvc.perform(
+            post("/api/scores")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)
+        )
+            .andExpect(status().isBadRequest)
+    }
+
+    @Test
+    fun `when Post score with more than 10 words then throw bad request with error`() {
+        val content = mapOf("word" to "morethantenwordsword", "score" to 10)
+        val objectMapper = ObjectMapper()
+        val json = objectMapper.writeValueAsString(content)
+
+        mockMvc.perform(
+            post("/api/scores")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)
+        )
+            .andExpect(status().isBadRequest)
+            .andDo(print())
+    }
+
 }
