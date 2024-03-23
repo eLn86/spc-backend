019d72063536700386be6eb0eab350f1e09bdcc3
Elnathan Erh <elnathan.erh@synpulse.com>
2024-03-24 00:11:32 +0800

    add custom method to findByWord

--- src/main/kotlin/com/spc/spcbackend/repository/ScoreRepository.kt
+++ src/main/kotlin/com/spc/spcbackend/repository/ScoreRepository.kt
@@ -4,4 +4,6 @@ import com.spc.spcbackend.model.Score
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
-interface ScoreRepository : JpaRepository<Score, UUID>
+interface ScoreRepository : JpaRepository<Score, UUID> {
+    fun findByWord(word: String): Optional<Score>
+}
