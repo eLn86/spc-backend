b216ba7d09e9f06d6c1b1e44352fc58bb5b46181
Elnathan Erh <elnathan.erh@synpulse.com>
2024-03-24 00:12:23 +0800

    amend logic to catch custom exception and return specific error message and HTTP status

--- src/main/kotlin/com/spc/spcbackend/controller/ScoreController.kt
+++ src/main/kotlin/com/spc/spcbackend/controller/ScoreController.kt
@@ -1,10 +1,11 @@
 package com.spc.spcbackend.controller
 
-import ScoreResponse
 import TopTenScoresResponse
+import WordAlreadyExistsException
 import com.spc.spcbackend.dto.ScoreRequest
 import com.spc.spcbackend.service.ScoreService
 import jakarta.validation.Valid
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
@@ -14,8 +15,15 @@ import java.util.*
 @RequestMapping("/api/scores")
 class ScoreController(private val scoreService: ScoreService) {
     @PostMapping
-    fun saveScore(@RequestBody @Valid request: ScoreRequest): ResponseEntity<ScoreResponse> =
-        ResponseEntity.ok(scoreService.saveScore(request))
+    fun saveScore(@RequestBody @Valid request: ScoreRequest): ResponseEntity<Any> {
+        return try {
+            val savedScoreResponse = scoreService.saveScore(request)
+            ResponseEntity.ok(savedScoreResponse)
+        } catch (ex: WordAlreadyExistsException) {
+            ResponseEntity.status(HttpStatus.CONFLICT)
+                .body(mapOf("error" to "Word already exists, save not successful."))
+        }
+    }
 
     @GetMapping
     fun getTopTenScores(): ResponseEntity<TopTenScoresResponse> =
