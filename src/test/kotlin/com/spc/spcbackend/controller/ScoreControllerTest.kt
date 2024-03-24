54157c5aa9bade8ddcc583512f0e7ab905a2a6f3
Elnathan Erh <elnathan.erh@synpulse.com>
2024-03-24 23:15:06 +0800

    add more tests for controller and service.

--- src/test/kotlin/com/spc/spcbackend/controller/ScoreControllerTest.kt
+++ src/test/kotlin/com/spc/spcbackend/controller/ScoreControllerTest.kt
@@ -1,6 +1,10 @@
 package com.spc.spcbackend.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import com.spc.spcbackend.model.Score
+import com.spc.spcbackend.repository.ScoreRepository
+import org.hamcrest.Matchers.hasSize
+import org.hamcrest.Matchers.`is`
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
@@ -8,17 +12,22 @@ import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
+
 
 @AutoConfigureMockMvc
 @SpringBootTest
 @ActiveProfiles("test")
 class ScoreControllerTest(@Autowired val mockMvc: MockMvc) {
+    @Autowired
+    private val scoreRepository: ScoreRepository? = null
+
     @Test
     fun `when Post score then Save score`() {
+        scoreRepository?.deleteAll()
         val content = mapOf("word" to "scrupulous", "score" to 16)
         val objectMapper = ObjectMapper()
         val json = objectMapper.writeValueAsString(content)
@@ -62,4 +71,31 @@ class ScoreControllerTest(@Autowired val mockMvc: MockMvc) {
             .andDo(print())
     }
 
+    @Test
+    fun `when Post score that already exists throw WordAlreadyExists error`() {
+        val content = mapOf("word" to "scrupulous", "score" to 16)
+        val objectMapper = ObjectMapper()
+        val json = objectMapper.writeValueAsString(content)
+
+        mockMvc.perform(
+            post("/api/scores")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json)
+        )
+            .andExpect(status().isConflict)
+            .andExpect(jsonPath("$.error", `is`("Word already exists, save not successful.")))
+    }
+
+    @Test
+    fun `getScores endpoint returns scores`() {
+        mockMvc.perform(
+            get("/api/scores")
+                .contentType(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(status().isOk)
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andExpect(jsonPath("$.scores", hasSize<Array<Score>>(1)))
+            .andExpect(jsonPath("$.scores[0].word", `is`("scrupulous")))
+            .andExpect(jsonPath("$.scores[0].score", `is`(16)))
+    }
 }
--- src/test/kotlin/com/spc/spcbackend/service/ScoreServiceTest.kt
+++ src/test/kotlin/com/spc/spcbackend/service/ScoreServiceTest.kt
@@ -1,5 +1,6 @@
 package com.spc.spcbackend.service
 
+import WordAlreadyExistsException
 import com.spc.spcbackend.dto.ScoreRequest
 import com.spc.spcbackend.model.Score
 import com.spc.spcbackend.repository.ScoreRepository
@@ -9,8 +10,10 @@ import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.verify
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.util.*
 
 class ScoreServiceTest {
@@ -33,6 +36,7 @@ class ScoreServiceTest {
             word = request.word,
             score = request.score
         )
+        every { scoreRepository.findByWord(any()) } returns Optional.empty()
         every { scoreRepository.save(any()) } returns savedScore
 
         val returnedSaveScore = scoreService.saveScore(request)
@@ -40,5 +44,43 @@ class ScoreServiceTest {
         verify { scoreRepository.save(any()) }
         assertEquals("example", returnedSaveScore.word)
         assertEquals(5, returnedSaveScore.score)
+    }
+
+    @Test
+    fun `when SaveScore and word exists then throw WordAlreadyExistsException`() {
+        val request = ScoreRequest(word = "example", score = 5)
+        val savedScore = Score(
+            id = UUID.randomUUID(),
+            word = request.word,
+            score = request.score
+        )
+        every { scoreRepository.findByWord(any()) } returns Optional.of(
+            Score(
+                word = "example",
+                score = 5,
+                id = UUID.randomUUID()
+            )
+        )
+        every { scoreRepository.save(any()) } returns savedScore
+
+        assertThrows<WordAlreadyExistsException> {
+            scoreService.saveScore(request)
+        }
+    }
+
+    @Test
+    fun `getTopTenScores returns top ten scores sorted by score descending`() {
+        val mockScores = (1..12).map {
+            Score(UUID.randomUUID(), "word$it", it)
+        }
+        every { scoreRepository.findAll() } returns mockScores
+
+        val result = scoreService.getTopTenScores()
+
+        verify { scoreRepository.findAll() }
+        assertEquals(10, result.scores.size)
+        assertTrue(result.scores[0].score >= result.scores[1].score)
+        assertEquals("word12", result.scores[0].word)
+        assertEquals("word3", result.scores.last().word)
     }
 }
