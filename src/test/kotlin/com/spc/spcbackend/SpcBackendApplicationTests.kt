43f0e4d543785b01e9f1ddd64b9c26a358f05f15
Elnathan Erh <elnathan.erh@synpulse.com>
2024-03-24 23:41:34 +0800

    add activeprofile

--- src/test/kotlin/com/spc/spcbackend/SpcBackendApplicationTests.kt
+++ src/test/kotlin/com/spc/spcbackend/SpcBackendApplicationTests.kt
@@ -4,10 +4,11 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpcBackendApplicationTests {
-
     @Test
     fun contextLoads(context: ApplicationContext) {
         assertThat(context).isNotNull()
